22285eb12d44f3a82b2592c7fb2871165dce404e
Yunda0204 <a01034353045@gmail.com>
2024-09-13 19:14:10 +0900

    Counselling 개인 Controller ADMIN으로 이사감
    수업내용

--- src/main/java/com/thxforservice/member/entities/Member.java
+++ src/main/java/com/thxforservice/member/entities/Member.java
@@ -12,7 +12,13 @@ import lombok.Data;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Member {
 
-    private Long seq;
+    /**
+     * 진짜 엔티티가 아니라 데이터 담는 거니깐 Member에
+     * 모든 직업 다 담아도 됌
+     *
+     */
+
+    private Long memberSeq;
 
     private String gid;
 
