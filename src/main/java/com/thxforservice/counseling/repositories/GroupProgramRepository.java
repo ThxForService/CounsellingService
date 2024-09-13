f5a1183aef3e1a6a62f87092e9d6ace7fda11405
yoojung275 <yoojung275@gmail.com>
2024-09-13 09:50:34 +0900

    GroupCounselingRepository, GroupProgramRepository 이름 꼬인 거 수정

--- src/main/java/com/thxforservice/counseling/repositories/GroupProgramRepository.java
+++ src/main/java/com/thxforservice/counseling/repositories/GroupProgramRepository.java
@@ -1,8 +1,9 @@
 package com.thxforservice.counseling.repositories;
 
-import com.thxforservice.counseling.entities.GroupCounseling;
+
+import com.thxforservice.counseling.entities.GroupProgram;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface GroupProgramRepository extends JpaRepository<GroupCounseling, Long>, QuerydslPredicateExecutor<GroupCounseling> {
+public interface GroupProgramRepository extends JpaRepository<GroupProgram, Long>, QuerydslPredicateExecutor<GroupProgram> {
 }
