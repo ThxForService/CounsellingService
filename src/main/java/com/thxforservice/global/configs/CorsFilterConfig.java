63816604540bbf0e45871c02be85e4142bf2520c
yoojung275 <yoojung275@gmail.com>
2024-09-23 12:02:01 +0900

    cors 소스 수정

--- src/main/java/com/thxforservice/global/configs/CorsFilterConfig.java
+++ src/main/java/com/thxforservice/global/configs/CorsFilterConfig.java
@@ -4,9 +4,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.util.StringUtils;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @Profile("!test") // 테스트에선 사용 X
@@ -25,13 +29,13 @@ public class CorsFilterConfig {
         config.addAllowedMethod("*"); // 모든 요청 메서드 허용
         config.addAllowedHeader("*"); // 모든 요청 헤더 허용
 
-        /*
-        if (!allowedOrigins.equals("*")) {
+        if (StringUtils.hasText(allowedOrigins)) {
+            List<String> origins = Arrays.stream(allowedOrigins.split(",")).toList();
+            config.setAllowedOrigins(origins);
             config.setAllowCredentials(true);
+        } else {
+            config.addAllowedOrigin("*");
         }
-        config.addAllowedOrigin(allowedOrigins);
-        */
-        config.addAllowedOrigin("*");
         config.addExposedHeader("*");
 
         source.registerCorsConfiguration("/**", config);
