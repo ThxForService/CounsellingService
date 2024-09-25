04fe7eb4cef123a9e4870dccc1b580c7a824db9c
yoojung275 <yoojung275@gmail.com>
2024-09-25 18:49:17 +0900

    집단 상담 신청통제

--- src/main/java/com/thxforservice/counseling/validators/GroupCounselingApplyValidator.java
+++ src/main/java/com/thxforservice/counseling/validators/GroupCounselingApplyValidator.java
@@ -2,18 +2,23 @@ package com.thxforservice.counseling.validators;
 
 import com.querydsl.core.BooleanBuilder;
 import com.thxforservice.counseling.controllers.RequestGroupCounselingApply;
+import com.thxforservice.counseling.entities.GroupProgram;
 import com.thxforservice.counseling.entities.QGroupCounseling;
 import com.thxforservice.counseling.repositories.GroupCounselingRepository;
+import com.thxforservice.counseling.repositories.GroupProgramRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
+import java.time.LocalDate;
+
 @Component
 @RequiredArgsConstructor
 public class GroupCounselingApplyValidator implements Validator {
 
-    private final GroupCounselingRepository repository;
+    private final GroupCounselingRepository counselingRepository;
+    private final GroupProgramRepository programRepository;
 
     @Override
     public boolean supports(Class<?> clazz) {
@@ -37,10 +42,30 @@ public class GroupCounselingApplyValidator implements Validator {
         builder.and(groupCounseling.studentNo.eq(studentNo))
                 .and(groupCounseling.program.pgmSeq.eq(pgmSeq));
 
-        if (repository.exists(builder)) {
+        if (counselingRepository.exists(builder)) {
             errors.reject("Exists");
         }
 
-        //
+        // 신청기간에 따라 신청 여부 통제
+        LocalDate now = LocalDate.now();
+        GroupProgram program = programRepository.findById(pgmSeq).orElse(null);
+
+        if (program != null) {
+            if (now.isBefore(program.getStartDate())) {
+                errors.reject("NotInApplicationPeriod");
+            } else if (now.isAfter(program.getEndDate())) {
+                errors.reject("NotInApplicationPeriod");
+            }
+
+            // 신청 정원
+            long currentCount = counselingRepository.count(groupCounseling.program.pgmSeq.eq(pgmSeq));
+
+            int capacity = program != null ? program.getCapacity() : 0;
+
+            if (currentCount > capacity) {
+                errors.reject("CapacityExceeded");
+            }
+        }
     }
 }
+
